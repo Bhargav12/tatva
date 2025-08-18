0723c5651b68ae37c163966467a365f1dda4fbf5
Bhargava <b.parthasarathy@globallogic.com>
2025-08-18 21:03:22 +0530

    build fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../App";
+import App from "./App";
 import "./styles/global.css"; // Global styles
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
