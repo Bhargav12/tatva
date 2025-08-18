a8a7bec1ad653f336e021e1701c841142e313b81
Bhargava <b.parthasarathy@globallogic.com>
2025-08-18 21:05:32 +0530

    build fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css"; // Global styles
+import "./styles/globals.css"; // Global styles
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
